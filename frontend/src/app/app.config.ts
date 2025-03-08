e5cfb73d9969dde4b21deca3707e771b93e67135
Luciano Borges <lucianosantosborges@gmail.com>
2025-03-08 10:02:20 +0000

    feat: Integrando o frontend com o Keycloak.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {provideKeycloak} from 'keycloak-angular';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideKeycloak({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'pragaas',
+        clientId: 'frontend'
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        checkLoginIframe: false
+      }
+    }),
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes)
+  ]
 };
